6403a78def448f6454af9b5dfa080ae17b934435
KelvinOyanna <dotkelplus@gmail.com>
2023-01-30 17:50:13 +0100

    added some proccess completion notification

--- Problem_2_Solution/main.py
+++ Problem_2_Solution/main.py
@@ -6,10 +6,12 @@ account_id = get_api_credentials()[0]
 api_key = get_api_credentials()[1]
 
 def main():
+    'Main function for running all other functions/modules.'
     # Pull exchange rates data from API
     get_exchange_rates(account_id, api_key)
     # Transform raw exchange rates data from an external JSON file
     transform_data()
-    print('Transformed data written to a csv file')
+    print('Transformed exchange rates data written to a csv file')
 
 main()
+
