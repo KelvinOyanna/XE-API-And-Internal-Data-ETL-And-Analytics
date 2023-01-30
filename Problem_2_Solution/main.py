ce06e73f36b1bdbed8494404c0d8d7f787fe7ed8
KelvinOyanna <dotkelplus@gmail.com>
2023-01-30 15:10:09 +0100

    updated the main module to get api credetials and run the get_exchange_rate function from main

--- Problem_2_Solution/main.py
+++ Problem_2_Solution/main.py
@@ -1,9 +1,15 @@
 from extract_xchange_rates import get_exchange_rates, transform_data
+from util import get_api_credentials
+
+# API credentials
+account_id = get_api_credentials()[0]
+api_key = get_api_credentials()[1]
 
 def main():
     # Pull exchange rates data from API
-    get_exchange_rates()
-    # Transform raw exchange rates data in an external JSON file
+    get_exchange_rates(account_id, api_key)
+    # Transform raw exchange rates data from an external JSON file
     transform_data()
+    print('Transformed data written to a csv file')
 
 main()
