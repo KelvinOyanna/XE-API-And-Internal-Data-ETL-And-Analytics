a24accd82b2a296735d59874f384f6cbb888a688
KelvinOyanna <dotkelplus@gmail.com>
2023-01-30 16:18:12 +0100

    update directory structure

--- main.py
+++
@@ -1,9 +0,0 @@
-from extract_xchange_rates import get_exchange_rates, transform_data
-
-def main():
-    # Pull exchange rates data from API
-    get_exchange_rates()
-    # Transform raw exchange rates data in an external JSON file
-    transform_data()
-
-main()
--- util.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-
-
-def check_last_updated(json_record, response_data):
-        last_updated = datetime.strptime(json_record[-1].get('timestamp'), '%Y-%m-%dT%H:%M:%SZ').date()
-        if (datetime.strptime(response_data.get('timestamp'), '%Y-%m-%dT%H:%M:%SZ').date()) != last_updated:
-            return True
-        else:
-            return False
