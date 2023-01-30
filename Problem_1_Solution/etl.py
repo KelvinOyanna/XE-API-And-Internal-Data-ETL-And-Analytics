2e813eda6668e3923ce6d9a3c7fd2360b8327043
KelvinOyanna <dotkelplus@gmail.com>
2023-01-30 23:01:47 +0100

    update etl logic

--- Problem_1_Solution/etl.py
+++ Problem_1_Solution/etl.py
@@ -14,32 +14,39 @@ from util import validate_date
 # Create database connection engine
 conn_engine = get_database_conn()
 
-# Extract data for all the tables
-borrower = pd.read_csv('data/Borrower_Data.csv')
-loan = pd.read_csv('data/Loan_Data.csv')
-payment_schedule = pd.read_csv('data/Payment_Schedule.csv')
-loan_payment = pd.read_csv('data/Loan_payment.csv')
+
 
 def transform_load_to_db():
+    '''
+    This function extract, transforms and loads data into a Postgresql database.
+    Parameter: Does not accept a parameter
+    Return value: Doest not return a value
+    Return type: None
+    '''
+    # Extract data for all the tables
+    borrower = pd.read_csv('data/Borrower_Data.csv')
+    loan = pd.read_csv('data/Loan_Data.csv')
+    payment_schedule = pd.read_csv('data/Payment_Schedule.csv')
+    loan_payment = pd.read_csv('data/Loan_payment.csv')
     # Transform and load borrower data to a table in the database
     borrower = borrower.loc[:, borrower.columns != 'borrower_credit_score']
     borrower.rename(columns= {'Borrower_Id': 'borrower_id', 'State': 'state', 'City': 'city', 'zip code': 'zip_code'}, inplace=True)
-    #borrower.to_sql('borrower', con = db_conn, if_exists = 'append', index = False)
+    borrower.to_sql('borrower', con = conn_engine, if_exists = 'append', index = False)
 
     # Transform and load loan table to database
     loan['Date_of_release'] = loan['Date_of_release'].apply(validate_date)
     loan['Maturity_date'] = loan['Maturity_date'].apply(validate_date)
     loan.rename(columns= {'Borrower_id': 'borrower_id', 'Date_of_release': 'date_of_release', 'Term': 'term', 'InterestRate': 'interest_rate', \
         'LoanAmount': 'loan_amount', 'Downpayment': 'down_payment', 'Payment_frequency': 'payment_frequency', 'Maturity_date': 'maturity_date'}, inplace=True)
-    #loan.to_sql('loan', con = db_conn, if_exists = 'append', index = False)
+    loan.to_sql('loan', con = conn_engine, if_exists = 'append', index = False)
 
     # Transform and load payment_schedule data to a table in the database
     payment_schedule['Expected_payment_date'] = payment_schedule['Expected_payment_date'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y').date())
     payment_schedule.rename(columns={'Expected_payment_date': 'expected_payment_date', 'Expected_payment_amount' : 'expected_payment_amount'}, inplace=True)
-    #payment_schedule.to_sql('payment_schedule', con = db_conn, if_exists = 'append', index = False)
+    payment_schedule.to_sql('payment_schedule', con = conn_engine, if_exists = 'append', index = False)
 
     # Transform and load loan_payment data to a table in the database
     loan_payment.rename(columns= {'loan_id(fk)': 'loan_id', 'payment_id(pk)': 'payment_id', 'Amount_paid': 'date_paid', \
         'Date_paid': 'amount_paid'}, inplace= True)
     loan_payment['date_paid'] = loan_payment['date_paid'].apply(lambda x: datetime.strptime(x, '%m/%d/%Y').date())
-    loan_payment.to_sql('loan_payment', con = db_conn, if_exists = 'append', index = False)
+    loan_payment.to_sql('loan_payment', con = conn_engine, if_exists = 'append', index = False)
