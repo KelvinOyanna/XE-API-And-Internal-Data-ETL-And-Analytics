7ed54b643344dcf77181633e8831c9f47eee2b5a
KelvinOyanna <dotkelplus@gmail.com>
2023-01-30 14:20:44 +0100

    added create_analytics_report function for creting analytics based on sql logic

--- Problem_1_Solution/util.py
+++ Problem_1_Solution/util.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+import pandas as pd
 from dotenv import dotenv_values
 from datetime import datetime
 dotenv_values()
@@ -10,7 +11,7 @@ def get_database_conn():
     database instance.
     Parameter: Does not accept a parameter
     Return value: return a postgresql database connection object
-    Return type: database obeject
+    Return type: Postgresql database obeject
     '''
     # Get database credentials from environment variable
     config = dict(dotenv_values('../.env'))
@@ -24,7 +25,30 @@ def get_database_conn():
     
 
 def validate_date(date_str):
+    '''
+    This function convert a date string value to a date object. It also checks for date validity
+    by converting February 29 to 28 for Non leap years.
+    Parameter: Accepts a date string
+    Return value: returns a transformed date object.
+    Return type: date
+    '''
     try:
         return datetime.strptime(date_str, '%m/%d/%Y').date()
     except ValueError:
         return datetime.strptime(date_str.replace('29', '28'), '%m/%d/%Y').date()
+
+def create_analytics_report(query, conn_engine, output_file_name):
+    '''
+    This function is used to generate analytics report from tables in the 
+    database based on specified query logic.
+    Parameters:
+     - query : The SQL query logic for the report.
+     - conn_engine : The is the database connection engine object
+     - output_file_name : The name of the created outpufile with it's path.
+    Return value: Does not return a value
+    Return type: None
+    '''
+    # Read data from database tables based on query logic
+    report = pd.read_sql(query, con = conn_engine, index_col= None)
+    # Write report data to an external .xls file
+    report.to_excel(output_file_name, index= False)
